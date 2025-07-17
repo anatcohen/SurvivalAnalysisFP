4e086ef4ba80c57644601e09d6c31f90e7c3556e
Itamar Tzafrir <itamar.tzafrir@aidoc.com>
2025-07-17 11:29:01 +0300

    preprocessed clinical data: numerical features instead of boolean

--- preprocessing/clinical_data/clinical_data_preprocessing.py
+++ preprocessing/clinical_data/clinical_data_preprocessing.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import numpy as np
 
 def load_and_preprocess(path):
     """
@@ -7,7 +7,7 @@ def load_and_preprocess(path):
     """
 
     # Load data
-    raw_data = pd.read_csv('data/clinical_data.csv')
+    raw_data = pd.read_csv(path)
 
     df = raw_data.copy(deep=True)
 
@@ -18,15 +18,27 @@ def load_and_preprocess(path):
     df['Histology'] = df['Histology'].fillna(df['Histology'].mode())
 
     # One-hot encode categorical data
-    df = pd.concat([raw_data, pd.get_dummies(raw_data['Overall.Stage'], prefix='stage'),
-                    pd.get_dummies(raw_data['Histology'], prefix='histology'),
-                    pd.get_dummies(raw_data['clinical.T.Stage'], prefix='T.stage'),
-                    pd.get_dummies(raw_data['Clinical.N.Stage'], prefix='N.stage'),
-                    pd.get_dummies(raw_data['Clinical.M.Stage'], prefix='M.stage')],
+    df = pd.concat([df, pd.get_dummies(raw_data['Overall.Stage'], prefix='stage', dtype=int),
+                    pd.get_dummies(raw_data['Histology'], prefix='histology', dtype=int),
+                    pd.get_dummies(raw_data['clinical.T.Stage'], prefix='T.stage', dtype=int),
+                    pd.get_dummies(raw_data['Clinical.N.Stage'], prefix='N.stage', dtype=int),
+                    pd.get_dummies(raw_data['Clinical.M.Stage'], prefix='M.stage', dtype=int)],
                    axis=1).drop(
-        columns=['Overall.Stage', 'Histology', 'clinical.T.Stage', 'Clinical.N.Stage', 'Clinical.M.Stage'])
-    df['is_male'] = df['gender'] == 'male'
+        columns=['Overall.Stage', 'Histology', 'clinical.T.Stage', 'Clinical.N.Stage', 'Clinical.M.Stage', 'gender'])
+    df['is_male'] = (raw_data['gender'] == 'male').astype(int)
 
-    # Normalise data
-    df['age'] = (df['age'] - df['age'].min()) / (df['age'].max() - df['age'].min())
     return df
+
+if __name__ == '__main__':
+    df = load_and_preprocess('../../data/clinical_data.csv')
+    df.to_csv('../../data/preprocessed_clinical_data.csv', index=False)
+
+    print(df)
+    nan_counts = df.isnull().sum()
+    columns_with_nans = nan_counts[nan_counts > 0]
+
+    if len(columns_with_nans) > 0:
+        print("Columns with NaN values:")
+        print(columns_with_nans)
+    else:
+        print("No NaN values found!")
